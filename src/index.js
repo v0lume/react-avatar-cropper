54597dcc3e122610bc9a62952afd1ab2b123cd61
Justin Schiff <AmbientRevolution@gmail.com>
2015-05-12 00:58:48 -0700

    simple bounding added.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import AvatarCropper from "../../lib";
 var App = React.createClass({
   render () {
     return (
-      <AvatarCropper image="http://placekitten.com/1920/900" width="400" height="400" />
+      <AvatarCropper image="http://placekitten.com/1920/900" width={400} height={400} />
     );
   }
 });
